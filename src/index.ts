915fd03f967da6061346673b1619fe2bd1b4a6e5
wangxingkang <wang_xingkang@qq.com>
2020-12-22 18:16:27 +0800

    fix: 修复水印可被删除

--- src/index.ts
+++ src/index.ts
@@ -17,23 +17,23 @@ const defaultOptions: Partial<WatermarkOptions> = {
   mode: 'repeat'
 }
 
+const attributeName = 'data-watermark';
 const observeOptions = {
   childList: true,
-  attributes: true,
   subtree: true,
-  attributeFilter: ['style'],
-  attributeOldValue: true
+  attributeFilter: ['style', attributeName]
 }
+const MutationObserver = getMutationObserver();
 
 class Watermark {
   private options: WatermarkOptions;
   private shadowRoot: ShadowRoot | HTMLElement | undefined;
   private watermarkParentDom: HTMLDivElement | undefined;
+  private containerParentNode: HTMLElement | null;
   private container: HTMLElement | undefined;
   private watermarkDom: HTMLElement | undefined;
-  private watermarkParentId: string;
   private watermarkId: string;
-  private mutationObserver: any;
+  private mutationObserver: MutationObserver | null;
   private style: {
     [key: string]: any;
   }
@@ -53,7 +53,8 @@ class Watermark {
     }
     this.style.zIndex = this.options.zIndex;
     this.watermarkId = genRandomId('watermark');
-    this.watermarkParentId = genRandomId('watermark');
+    this.mutationObserver = null;
+    this.containerParentNode = null;
   }
 
   /**
@@ -65,15 +66,152 @@ class Watermark {
     this.style.zIndex = this.options.zIndex;
 
     if (!this.options.monitor) {
-      this.mutationObserver?.disconnect();
-      this.mutationObserver = undefined;
+      this._destroyMutationObserver();
     }
   }
 
+  /**
+   * 绘制水印
+   */
+  render() {
+    const { container } = this.options;
+    this.container = this._getContainer(container);
+    // TODO: 水印包裹展示内容是否更好
+    if (!this.containerParentNode) {
+      this.containerParentNode = this.container.parentNode as HTMLElement;
+    }
+    if (!this.container.parentNode && this.containerParentNode) {
+      this.containerParentNode.append(this.container);
+    }
+
+    this._destroyMutationObserver();
+
+    // 解决滚动区域无水印问题
+    let height = this._getWatermarkHeight();
+    // 获取水印DOM
+    const watermaskDom = this._getWatermarkDom(height);
+    watermaskDom.setAttribute(attributeName, this.watermarkId)
+
+
+    // 删除已有水印
+    if (this.watermarkParentDom) {
+      const childs = this.watermarkParentDom.childNodes || [];
+
+      for(let i = childs.length - 1; i >= 0; i--) {
+        this.watermarkParentDom.removeChild(childs[i]);
+      }
+    }
+
+    if (!this.watermarkParentDom) {
+      // 水印父节点
+      this.watermarkParentDom = document.createElement('div');
+      this.watermarkParentDom.setAttribute('style', getStyleStr({
+        pointerEvents: 'none'
+      }));
+
+      // 判断是否支持 Shadow DOM
+      if (typeof this.watermarkParentDom.attachShadow === 'function') {
+        this.shadowRoot = this.watermarkParentDom.attachShadow({
+          mode: 'open'
+        });
+      } else {
+        this.shadowRoot = this.watermarkParentDom;
+      }
+    } else if (this.watermarkParentDom.shadowRoot) {
+      this.shadowRoot =this.watermarkParentDom.shadowRoot;
+    }
+    this.watermarkParentDom.setAttribute(attributeName, this.watermarkId)
+
+    this.container.appendChild(this.watermarkParentDom);
+    this.shadowRoot?.append(watermaskDom);
+
+    if (MutationObserver && this.options.monitor) {
+      this.mutationObserver = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+          if (this._isAgainRender(mutation)) {
+            this.render();
+            return;
+          }
+        })
+      });
+      this.mutationObserver.observe(this.container, observeOptions);
+      this.watermarkParentDom.shadowRoot && this.mutationObserver.observe(this.watermarkParentDom.shadowRoot, observeOptions);
+    }
+  }
+
+  /**
+   * 销毁水印
+   */
+  destroy() {
+    this.container = undefined;
+
+    this.watermarkDom?.remove();
+    this.watermarkParentDom?.remove();
+    this._destroyMutationObserver();
+  }
+
+  /**
+   * 判断是否发起重新生成水印
+   * @param mutation
+   */
+  _isAgainRender = (mutation: MutationRecord) => {
+    if (mutation.type === 'attributes') {
+      if (mutation.attributeName === 'data-watermark') {
+        return true;
+      }
+      if (this.watermarkId === this.getNodeRandomId(mutation.target)) {
+        return true;
+      }
+    }
+
+    if (mutation.removedNodes.length) {
+      if (this.watermarkId === (this.getNodeRandomId(mutation.removedNodes[0]))) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  getNodeRandomId = (node: Node) => {
+    return node?.['dataset']?.['watermark'];
+  }
+
+  /**
+   * 销毁MutationObserver
+   */
+  _destroyMutationObserver = () => {
+    if (this.mutationObserver) {
+      this.mutationObserver.disconnect();
+      this.mutationObserver = null;
+    }
+  }
+
+  /**
+   * 获取水印挂载节点
+   * @param container
+   */
+  _getContainer = (container: WatermarkOptions['container']): HTMLElement  => {
+    let dom: HTMLElement | undefined;
+
+    if (typeof container === 'string') {
+      dom = document.getElementById(container) as HTMLElement;
+      !dom && new Error(`水印挂载节点未找到，请检查#${container}是否存在`);
+      return dom;
+    } else {
+      container?.setAttribute(attributeName, this.watermarkId)
+    }
+
+    return container || document.body
+  }
+
+  /**
+   * 获取水印节点
+   * @param height
+   */
   _getWatermarkDom = (height: number) => {
     if (!this.watermarkDom) {
       this.watermarkDom = document.createElement('div');
-      this.watermarkDom.setAttribute('data-1', this.watermarkId)
     }
 
     const styles: AnyObj = {
@@ -103,125 +241,20 @@ class Watermark {
     return this.watermarkDom;
   }
 
-  /**
-   * 获取水印挂载节点
-   * @param container
-   */
-  _getContainer = (container: WatermarkOptions['container']): HTMLElement  => {
-    let dom: HTMLElement | undefined;
-
-    if (typeof container === 'string') {
-      dom = document.getElementById(container) as HTMLElement;
-      !dom && new Error(`水印挂载节点未找到，请检查#${container}是否存在`);
-      return dom;
-    }
-
-    return container || document.body
-  }
-
-  /**
-   * 绘制水印
-   */
-  render() {
-    this.container = this._getContainer(this.options.container);
-    const MutationObserver = getMutationObserver();
-
-    this.mutationObserver?.disconnect();
-
+  _getWatermarkHeight = () => {
+    if (!this.container) return 0;
     // 解决滚动区域无水印问题
     let height = 0;
-    let scrollHeight: number = 0;
-    let clientHeight: number = 0;
 
-    if (this.options.container) {
-      if (this.container.parentNode) {
-        scrollHeight = this.container.parentNode?.['scrollHeight'];
-        clientHeight = this.container.parentNode?.['clientHeight'];
-      }
-    } else {
-      scrollHeight = this.container.scrollHeight;
-      clientHeight = this.container.clientHeight;
-    }
-    if (scrollHeight && clientHeight && scrollHeight > clientHeight) {
+    const { scrollHeight = 0, clientHeight = 0 }: HTMLElement = this.options.container
+      ? this.container.parentNode as HTMLElement
+      : this.container
+
+    if (scrollHeight > clientHeight) {
       height = Math.max(scrollHeight, clientHeight);
     }
 
-    // 获取水印DOM
-    const watermaskDom = this._getWatermarkDom(height);
-
-    // 删除已有水印
-    if (this.watermarkParentDom) {
-      const childs = this.watermarkParentDom.childNodes || [];
-
-      for(let i = childs.length - 1; i >= 0; i--) {
-        this.watermarkParentDom.removeChild(childs[i]);
-      }
-    }
-
-    if (!this.watermarkParentDom) {
-      // 水印父节点
-      this.watermarkParentDom = document.createElement('div');
-      this.watermarkParentDom.setAttribute('data-1', this.watermarkId)
-      this.watermarkParentDom.setAttribute('style', getStyleStr({
-        pointerEvents: 'none'
-      }));
-
-      // 判断是否支持 Shadow DOM
-      if (typeof this.watermarkParentDom.attachShadow === 'function') {
-        this.shadowRoot = this.watermarkParentDom.attachShadow({
-          mode: 'open'
-        });
-      } else {
-        this.shadowRoot = this.watermarkParentDom;
-      }
-    } else if (this.watermarkParentDom.shadowRoot){
-      this.shadowRoot =this.watermarkParentDom.shadowRoot;
-    }
-
-    this.container.appendChild(this.watermarkParentDom);
-    this.shadowRoot?.append(watermaskDom);
-
-    if (MutationObserver && this.options.monitor) {
-      // 直接删除容器节点，无法重新生成
-      this.mutationObserver = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-          // 修改样式则重新生成
-          if (
-            mutation.type === 'attributes' &&
-            [this.watermarkId, this.watermarkParentId].includes(this.getNodeRandomId(mutation.target))
-          ) {
-            this.render();
-            return;
-          }
-
-          if (
-            mutation.removedNodes.length &&
-            [this.watermarkId, this.watermarkParentId].includes((this.getNodeRandomId(mutation.removedNodes[0])))
-          ) {
-            this.render();
-            return;
-          }
-        })
-      });
-      this.mutationObserver.observe(this.container, observeOptions);
-      this.watermarkParentDom.shadowRoot && this.mutationObserver.observe(this.watermarkParentDom.shadowRoot, observeOptions);
-    }
-  }
-
-  getNodeRandomId = (node: Node) => {
-    return node?.['dataset']?.['1'];
-  }
-
-  /**
-   * 销毁水印
-   */
-  destroy() {
-    this.container = undefined;
-
-    this.watermarkDom?.remove();
-    this.watermarkParentDom?.remove();
-    this.mutationObserver?.disconnect();
-    this.mutationObserver = undefined;
+    return height;
   }
 }
 
