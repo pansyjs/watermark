59425e9dc1ef6febf0a126c834b0bd8eda30fe1d
wangxingkang <wang_xingkang@qq.com>
2021-01-15 15:49:51 +0800

    chore: 优化编译配置

--- .wbrc.ts
+++ .wbrc.ts
@@ -1,7 +1,10 @@
 import { Config } from '@walrus/build-types';
 
 const config: Config = {
-  banner: true
+  banner: true,
+  typeExtractor: {
+    files: ['index.d.ts']
+  }
 }
 
 export default config;
