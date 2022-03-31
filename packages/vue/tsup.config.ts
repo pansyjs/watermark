191f4af0ec0ad992a7795b2e5f75589bc22fad61
haiweilian <haiweilian@foxmail.com>
2022-03-31 15:19:49 +0800

    feat: add vue2/3

---
+++ packages/vue/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  sourcemap: true,
+  format: ['cjs', 'esm'],
+  target: 'es6',
+  clean: true,
+  dts: true,
+  platform: 'browser',
+  legacyOutput: true,
+})
